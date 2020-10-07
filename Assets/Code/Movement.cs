83f4df49baa6e5f4ba9e4dc216b1fb6f4dfea8e0
A00269349 <a00269349@student.ait.ie>
2020-10-07 17:03:19 +0100

    CleanUp

---
+++ Assets/Code/Movement.cs
@@ -0,0 +1,320 @@
+﻿using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+
+public class Movement : MonoBehaviour
+{
+    #region Variables
+
+    private Collision coll;
+    [HideInInspector]
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Stats")]
+    public float speed = 10;
+    public float jumpForce = 50;
+    public float slideSpeed = 5;
+    public float wallJumpLerp = 10;
+    public float dashSpeed = 20;
+
+    [Space]
+    [Header("Booleans")]
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+    [Space]
+    private bool groundTouch;
+    private bool hasDashed;
+
+    [Space]
+    [Header("Wall Side")]
+    public int side = 1;
+
+    [Space]
+    [Header("World Settings")]
+    public float gravityScale = 3f;
+    [Space]
+    [Header("FX")]
+    public ParticleSystem dashFX;
+    public ParticleSystem wallSlideFX;
+    public ParticleSystem landingFX;
+
+
+    #endregion Variables
+
+    private void Start()
+    {
+        coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // UPDATE VARIABLES //
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        // WALKING //
+        Walk(dir);
+
+        // START WALL GRAB / SLIDE //
+        if (coll.onWall && Input.GetButton("Fire3") && canMove)
+        {
+            wallGrab = true;
+            wallSlide = false;
+        }
+
+        // END WALL GRAB / SLIDE //
+        if (Input.GetButtonUp("Fire3") || !coll.onWall || !canMove)
+        {
+            wallGrab = false;
+            wallSlide = false;
+        }
+
+        // END WALL JUMP //
+        if (coll.onGround && !isDashing)
+        {
+            wallJumped = false;
+            GetComponent<Jumping>().enabled = true;
+        }
+
+        // SET GRAVITY SCALE FOR WALL WALL CLIMB //
+        if (wallGrab && !isDashing)
+        {
+            rb.gravityScale = 0;
+            if (x > .2f || x < -.2f)
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+
+            float speedModifier = y > 0 ? .5f : 1;
+
+            rb.velocity = new Vector2(rb.velocity.x, y * (speed * speedModifier));
+        }
+        else
+        {
+            rb.gravityScale = 3;
+        }
+
+        // START WALL SLIDE //
+        if (coll.onWall && !coll.onGround)
+        {
+            if (x != 0 && !wallGrab)
+            {
+                wallSlide = true;
+                WallSlide();
+            }
+        }
+
+        // END WALL SLIDE //
+        if (!coll.onWall || coll.onGround)
+        {
+            wallSlide = false;
+        }
+
+        // JUMP //
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (coll.onGround)
+            {
+                Jump(Vector2.up, false);
+
+            }
+            if (coll.onWall && !coll.onGround)
+            {
+                WallJump();
+            }
+        }
+
+        // DASH //
+        if (Input.GetButtonDown("Fire1") && !hasDashed)
+        {
+            if (xRaw != 0 || yRaw != 0)
+            {
+                CreateDashFX();
+                Dash(xRaw, yRaw);
+            }
+        }
+
+        // SET GROUND TOUCH //
+        if (coll.onGround && !groundTouch)
+        {
+            GroundTouch();
+            groundTouch = true;
+        }
+
+        // RESET TOUCH GROUND //
+        if (!coll.onGround && groundTouch)
+        {
+            groundTouch = false;
+        }
+
+        // HANDLE ERRORS //
+        if (wallGrab || wallSlide || !canMove)
+        {
+            return;
+        }
+
+        // HANDLE SIDES //
+        if (x > 0)
+        {
+            side = 1;
+        }
+        if (x < 0)
+        {
+            side = -1;
+        }
+    }
+
+    private void GroundTouch()
+    {
+        hasDashed = false;
+        isDashing = false;
+    }
+
+    private void WallSlide()
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        bool pushingWall = false;
+        if ((rb.velocity.x > 0 && coll.onRightWall) || (rb.velocity.x < 0 && coll.onLeftWall))
+        {
+            pushingWall = true;
+        }
+        float push = pushingWall ? 0 : rb.velocity.x;
+
+        rb.velocity = new Vector2(push, -slideSpeed);
+    }
+
+    private void WallJump()
+    {
+        if ((side == 1 && coll.onRightWall) || side == -1 && !coll.onRightWall)
+        {
+            side *= -1;
+        }
+
+        StopCoroutine(DisableMovement(0));
+        StartCoroutine(DisableMovement(.05f));
+
+        Vector2 wallDir = coll.onRightWall ? Vector2.left : Vector2.right;
+
+        Jump((Vector2.up / 1.5f + wallDir / 1.5f), true);
+
+        wallJumped = true;
+    }
+
+    private void Walk(Vector2 dir)
+    {
+        if (!canMove)
+        {
+            return;
+        }
+
+        if (wallGrab)
+        {
+            return;
+        }
+
+        if (!wallJumped)
+        {
+            rb.velocity = new Vector2(dir.x * speed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+        }
+    }
+
+    private void Jump(Vector2 dir, bool wall)
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += dir * jumpForce;
+    }
+
+    private void Dash(float x, float y)
+    {
+        hasDashed = true;
+
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+
+        rb.velocity += dir.normalized * dashSpeed;
+        
+        StartCoroutine(DashWait());
+    }
+
+    private IEnumerator DashWait()
+    {
+        // FindObjectOfType<GhostTrail>().ShowGhost();
+        StartCoroutine(GroundDash());
+        DOVirtual.Float(14, 0, .8f, RigidbodyDrag);
+
+        // dashParticle.Play();
+        rb.gravityScale = 0;
+        GetComponent<Jumping>().enabled = false;
+        wallJumped = true;
+        isDashing = true;
+
+        yield return new WaitForSeconds(.3f);
+
+        // dashParticle.Stop();
+        rb.gravityScale = 3;
+        GetComponent<Jumping>().enabled = true;
+        wallJumped = false;
+        isDashing = false;
+    }
+
+    private IEnumerator GroundDash()
+    {
+        yield return new WaitForSeconds(.15f);
+        if (coll.onGround)
+            hasDashed = false;
+    }
+
+    private IEnumerator DisableMovement(float time)
+    {
+        canMove = false;
+        yield return new WaitForSeconds(time);
+        canMove = true;
+    }
+
+    void RigidbodyDrag(float x)
+    {
+        rb.drag = x;
+    }
+
+    int ParticleSide()
+    {
+        int particleSide = coll.onRightWall ? 1 : -1;
+        return particleSide;
+    }
+
+    void WallParticle(float vertical)
+    {
+        var main = wallSlideFX.main;
+
+        if (wallSlide || (wallGrab && vertical < 0))
+        {
+            wallSlideFX.transform.parent.localScale = new Vector3(ParticleSide(), 1, 1);
+            main.startColor = Color.white;
+        }
+        else
+        {
+            main.startColor = Color.clear;
+        }
+    }
+
+    void CreateDashFX()
+    {
+        dashFX.Play();
+    }
+    
+}
